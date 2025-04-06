107c40bc6ff66d833bf53bb77c02fe0f4950059f
Bruno Ferreira <brunoferr2006@gmail.com>
2025-04-06 16:49:53 -0300

    Adiciona execução de cadastro de receita no Main.java

--- src/br/com/orcamento/app/Main.java
+++ src/br/com/orcamento/app/Main.java
@@ -1,7 +1,14 @@
 package br.com.orcamento.app;
 
+import br.com.orcamento.dao.ReceitaDAO;
+import br.com.orcamento.model.Receita;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Controle de Orçamento Pessoal Iniciado.");
+
+        Receita r = new Receita(2500.00, "Salário", "Salário de abril", "2025-04-06");
+        ReceitaDAO dao = new ReceitaDAO();
+        dao.inserirReceita(r);
     }
 }
